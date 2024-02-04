2fd3d7755522adc868a03c5bf0715245bc74bf6b
DenisDorogov <denisdorogov@yandex.ru>
2024-02-04 14:47:08 +0500

    Добавлен расчёт рождённых в этом году

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 
+const config = {
+  birthRate: [
+    {minAge: 15, maxAge: 19, value: 18.4},
+    {minAge: 20, maxAge: 24, value: 81.2},
+    {minAge: 25, maxAge: 29, value: 100.1},
+    {minAge: 30, maxAge: 34, value: 77.2},
+    {minAge: 35, maxAge: 39, value: 39.2},
+    {minAge: 40, maxAge: 44, value: 8.7},
+    {minAge: 45, maxAge: 49, value: 0.5},
+  ]
+}
+
 let startPopulation = [
   {age: 4, males: 20, females: 10},
   {age: 3, males: 30, females: 20},
@@ -11,6 +23,21 @@ const dataGenerator = () => {
   startPopulation.forEach(peoples => {
     if (maxPopulationByGender < peoples.males) maxPopulationByGender = peoples.males;
     if (maxPopulationByGender < peoples.females) maxPopulationByGender = peoples.females;
+  })
+  render();
+}
+
+const findBirthRate = (age) => {
+  let index = config.birthRate.findIndex(el => {
+    if (el.minAge <= age || el.maxAge >= age) return true;
+  })
+  return config.birthRate[index];
+}
+
+const lastYearBirthCalculator = () => {
+  let result = 0;
+  startPopulation.forEach(peoples => {
+    result += Math.round(findBirthRate(peoples.age) * peoples.females)
   })
 }
 
@@ -35,6 +62,6 @@ const render = () => {
 }
 
 dataGenerator();
-render();
+
 
 
